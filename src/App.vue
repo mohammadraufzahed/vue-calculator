ff7debfc65d080d24e65a54322345823e122c17c
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2021-04-28 17:08:38 +0430

    Add the basic calculator

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-6 m-auto text-center mt-5 rounded-3">
+        <div class="bg-light w-100 h-100 border border-dark">
+          <div class="mb-3">
+            <label for="inputOne">Input One</label>
+            <input
+              type="text"
+              class="form-control"
+              id="inputOne"
+              v-model.number="inputOne"
+            />
+          </div>
+          <div class="mb-3">
+            <label for="inputTwo">Input two</label>
+            <input
+              type="text"
+              class="form-control"
+              id="inputTwo"
+              v-model.number="inputTwo"
+            />
+          </div>
+          <div class="mb-3 mt-3">
+            <Button text="Add" class="btn-dark" @click="add()" />
+            <Button text="Subtract" class="btn-dark" @click="sub()" />
+            <Button text="Multiply" class="btn-dark" @click="multi()" />
+            <Button text="Divid" class="btn-dark" @click="divid()" />
+          </div>
+          <div>
+            <span>Result = {{ result }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Button from "./components/Button";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Button,
+  },
+  data() {
+    return {
+      inputOne: 0,
+      inputTwo: 0,
+      result: 0,
+    };
+  },
+  methods: {
+    add() {
+      this.result = this.inputOne + this.inputTwo;
+    },
+    sub() {
+      this.result = this.inputOne - this.inputTwo;
+    },
+    multi() {
+      this.result = this.inputOne * this.inputTwo;
+    },
+    divid() {
+      this.result = this.inputOne / this.inputTwo;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,
+body {
+  width: 100%;
+  height: 100%;
 }
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,14 @@
+<template>
+  <button class="btn ms-1">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    buttonType: String,
+  },
+  methods: {},
+};
+</script>
